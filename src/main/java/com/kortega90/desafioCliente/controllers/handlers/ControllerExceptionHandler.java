14781c5653e9b74a74354ccdf632e122e6660a80
kortega90 <kortega90@hotmail.com>
2022-12-21 13:37:36 -0300

    final exceptions

--- src/main/java/com/kortega90/desafioCliente/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/kortega90/desafioCliente/controllers/handlers/ControllerExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.kortega90.desafioCliente.controllers.handlers;
 
 import com.kortega90.desafioCliente.dto.CustomError;
+import com.kortega90.desafioCliente.dto.ValidationError;
 import com.kortega90.desafioCliente.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,7 +25,10 @@ import java.time.Instant;
         @ExceptionHandler(MethodArgumentNotValidException.class)
         public ResponseEntity<CustomError> argumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
             HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
-            CustomError err = new CustomError(Instant.now(),status.value(),e.getMessage(), request.getRequestURI());
+            ValidationError err = new ValidationError(Instant.now(),status.value(),"Dados inválidos", request.getRequestURI());
+            for (FieldError f: e.getBindingResult().getFieldErrors()){
+                 err.addError(f.getField(), f.getDefaultMessage());
+            }
             return ResponseEntity.status(status).body(err);
         }
 
